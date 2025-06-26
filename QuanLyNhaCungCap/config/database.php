580e7b8a4f1f4979d053a812134cd6d33a095911
ĐINH VIẾT CHIẾN <137360812+DinhVietChien1108@users.noreply.github.com>
2025-06-26 07:11:58 +0700

    Add files via upload

---
+++ QuanLyNhaCungCap/config/database.php
@@ -0,0 +1,23 @@
+<?php
+class Database {
+    private $host = '52.232.189.243';
+    private $db_name = 'toanphat_supplier_management';
+    private $username = 'zap';
+    private $password = 'Meobeo123@';
+    private $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->conn->exec("set names utf8");
+        } catch(PDOException $exception) {
+            echo "Connection error: " . $exception->getMessage();
+        }
+
+        return $this->conn;
+    }
+}
+?>
